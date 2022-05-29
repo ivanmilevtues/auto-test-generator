0377681465ffa9715d56e0b0bfd3be059e61a029
ivanmilevtues <ivanmilevtues@gmail.com>
2022-05-29 19:52:51 +0300

    Better generation of tests, Better prompt selection.

--- Model.py
+++ Model.py
@@ -10,11 +10,17 @@ from history_scanner.GitHistoryDataSetParser import GitHistoryDataSetParser
 class Model:
     def __init__(self):
         self.line_regex = r"line (\d+)"
-        self.prompt = None
+        self.original_prompt = None
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
     def create_test(self, prompt):
-        self.prompt = prompt
+        self.original_prompt = prompt
+        completion = self.complete(self.original_prompt)
+        code_with_test = self.complete_test(completion)
+        return self.__compile_code(code_with_test)
+
+    @staticmethod
+    def complete(prompt):
         response = openai.Completion.create(
             engine="code-davinci-002",
             prompt=prompt,
@@ -26,8 +32,15 @@ class Model:
             echo=True,
             stop=["#"]
         )
-        completion_text = response.choices[0]["text"]
-        return self.__compile_code(completion_text)
+        return response.choices[0]['text']
+
+    def complete_test(self, code):
+        generated_code = code[:len(self.original_prompt)]
+        if 'assert' in generated_code:
+            return code
+        if 'test_' in generated_code:
+            return self.complete(code + '\n# Generate asserts')
+        return self.complete(code + f'\n# Generate test case method\n{4*" "}def test_')
 
     def __compile_code(self, code, depth=150):
         if depth == 0:
@@ -46,21 +59,21 @@ class Model:
             print(code)
 
     def __get_only_generated_code(self, code):
-        return code[len(self.prompt):]
+        return code[len(self.original_prompt):]
 
 
 def main():
     parser = GitHistoryDataSetParser("./")
     data = parser.load_data("history_scanner/test_save.dat")
-    messages = MessageBuilder(data[:5])
+    messages = MessageBuilder(data, train=1)
     model = Model()
-    for message in messages.create_prompt():
-        test = model.create_test(message)
-        print(f"""
-        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-        The commit message is: {message}
-        Generated test is:\n{test}
-        """)
+    for prompts in messages.create_prompt():
+        for prompt in prompts:
+            try:
+                test = model.create_test(prompt)
+                print(f"Generated test is:\n{test}")
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
