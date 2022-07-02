124e6ef59501c6ca63f2b7bbcc65b2a386d03621
ivanmilevtues <ivanmilevtues@gmail.com>
2022-07-03 02:09:23 +0300

    E2E

--- generate_tests_code.py
+++ generate_tests_code.py
@@ -1,52 +1,35 @@
-# import uuid
-# from pathlib import Path
+import uuid
+from pathlib import Path
 import os
 
-from git import GitCommandError
-#
-# from generation.CodeCleanser import CodeCleanser
-# from generation.Compilable import Compilable
-# from generation.GeneratedTestSaver import GeneratedTestSaver
-# from generation.Generator import Generator
-# from generation.ImportResolver import ImportResolver
-# from history_scanner.GitHistoryDataSetParser import GitHistoryDataSetParser
-#
-#
-# def generate_test(branch):
-#     path = Path('./')
-#     setup_command = ''
-#     parser = GitHistoryDataSetParser(str(path.absolute()), branch=f'{branch}', only_last_commit=True)
-#     data = parser.get_parsed_data()
-#
-#     generator = Generator(CodeCleanser(str(path.absolute()), setup_command, ImportResolver(str(path)), Compilable()))
-#
-#     for commit in data:
-#         saver = GeneratedTestSaver(str(path.absolute()), commit.commit_id, main_branch='master',
-#                                    directory_for_generation=f'generated_tests_for_{commit.commit_id}')
-#         for prompt in commit.construct_prompt():
-#             try:
-#                 tests = generator.generate(prompt)
-#                 for test in tests:
-#                     saver.save_test_file(test, f'{uuid.uuid4()}')
-#             except Exception as e:
-#                 print(f"Tests for {prompt} not saved", e)
+from generation.CodeCleanser import CodeCleanser
+from generation.Compilable import Compilable
+from generation.GeneratedTestSaver import GeneratedTestSaver
+from generation.Generator import Generator
+from generation.ImportResolver import ImportResolver
+from history_scanner.GitHistoryDataSetParser import GitHistoryDataSetParser
+
+
+def generate_test(branch):
+    path = Path('./')
+    setup_command = ''
+    parser = GitHistoryDataSetParser(str(path.absolute()), branch=f'{branch}', only_last_commit=True)
+    data = parser.get_parsed_data()
+
+    generator = Generator(CodeCleanser(str(path.absolute()), setup_command, ImportResolver(str(path)), Compilable()))
+
+    for commit in data:
+        saver = GeneratedTestSaver(str(path.absolute()), commit.commit_id, main_branch='master',
+                                   directory_for_generation=f'generated_tests_for_{commit.commit_id}')
+        for prompt in commit.construct_prompt():
+            try:
+                tests = generator.generate(prompt)
+                for test in tests:
+                    saver.save_test_file(test, f'{uuid.uuid4()}')
+            except Exception as e:
+                print(f"Tests for {prompt} not saved", e)
 
 
 if __name__ == "__main__":
     branch_name = os.getenv("GITHUB_HEAD_REF")
-    # generate_test(branch_name)
-    from pydriller import Repository
-    repo = Repository('./',
-                      only_in_branch=branch_name,
-                      only_modifications_with_file_types=[".py"],
-                      include_deleted_files=False,
-                      order='reverse')
-    try:
-        for commit in repo.traverse_commits():
-            print(commit.hash, commit.msg, commit.in_main_branch)
-    except GitCommandError as e:
-        print('Command', e.command)
-        print('Stdout', e.stdout)
-        print('Stderr', e.stderr)
-        print('Status', e.status)
-        print('Args', e.args)
+    generate_test(branch_name)
