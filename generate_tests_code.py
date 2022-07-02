afa414aa80e39fac101ca7708225afa932c8f886
ivanmilevtues <ivanmilevtues@gmail.com>
2022-07-02 21:06:07 +0300

    E2E run

--- generate_tests_code.py
+++ generate_tests_code.py
@@ -1,33 +1,34 @@
-# import uuid
-# from pathlib import Path
-#
-# from generation.CodeCleanser import CodeCleanser
-# from generation.Compilable import Compilable
-# from generation.GeneratedTestSaver import GeneratedTestSaver
-# from generation.Generator import Generator
-# from generation.ImportResolver import ImportResolver
-# from history_scanner.GitHistoryDataSetParser import GitHistoryDataSetParser
+import uuid
+from pathlib import Path
+
+from generation.CodeCleanser import CodeCleanser
+from generation.Compilable import Compilable
+from generation.GeneratedTestSaver import GeneratedTestSaver
+from generation.Generator import Generator
+from generation.ImportResolver import ImportResolver
+from history_scanner.GitHistoryDataSetParser import GitHistoryDataSetParser
 
 import os
 
-# def generate_test(branch):
-#     path = Path('./')
-#     setup_command = ''
-#     parser = GitHistoryDataSetParser(str(path.absolute()), branch=branch, only_last_commit=True)
-#     data = parser.get_parsed_data()
-#
-#     generator = Generator(CodeCleanser(str(path.absolute()), setup_command, ImportResolver(str(path)), Compilable()))
-#
-#     for commit in data:
-#         saver = GeneratedTestSaver(str(path.absolute()), commit.commit_id, main_branch='master',
-#                                    directory_for_generation=f'generated_tests_for_{commit.commit_id}')
-#         for prompt in commit.construct_prompt():
-#             try:
-#                 tests = generator.generate(prompt)
-#                 for test in tests:
-#                     saver.save_test_file(test, f'{uuid.uuid4()}')
-#             except Exception as e:
-#                 print(f"Tests for {prompt} not saved", e)
+
+def generate_test(branch):
+    path = Path('./')
+    setup_command = ''
+    parser = GitHistoryDataSetParser(str(path.absolute()), branch=branch, only_last_commit=True)
+    data = parser.get_parsed_data()
+
+    generator = Generator(CodeCleanser(str(path.absolute()), setup_command, ImportResolver(str(path)), Compilable()))
+
+    for commit in data:
+        saver = GeneratedTestSaver(str(path.absolute()), commit.commit_id, main_branch='master',
+                                   directory_for_generation=f'generated_tests_for_{commit.commit_id}')
+        for prompt in commit.construct_prompt():
+            try:
+                tests = generator.generate(prompt)
+                for test in tests:
+                    saver.save_test_file(test, f'{uuid.uuid4()}')
+            except Exception as e:
+                print(f"Tests for {prompt} not saved", e)
 
 
 if __name__ == "__main__":
@@ -36,4 +37,4 @@ if __name__ == "__main__":
     print(os.getenv('OPENAI_API_KEY'))
     with open('test_code.py', 'w') as f:
         f.write('print("this is test")')
-    # generate_test(branch_name)
+    generate_test(branch_name)
